b290a61837eef9d9bb94eab36489841daae9813e
sahinmelikov <1rima621melikov@gmail.com>
2023-04-05 03:09:08 +0400

    Ab-201 Task METHOD OR FUNCTION TASK

---
+++ metodsAb201/Program.cs
@@ -0,0 +1,82 @@
+﻿#region Tek ve Cut
+//Console.WriteLine("Eded daxil edin");
+//int num=Convert.ToInt32(Console.ReadLine());
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine(num + " " + "Cut ededdir");
+//}
+//else
+//{
+//    Console.WriteLine(num +" "+ "tek ededdir");
+//}
+
+//Console.WriteLine("Eded daxil edin");
+//int num=Convert.ToInt32(Console.ReadLine());
+
+//for (int i =2; num > i; i++) 
+//{
+
+//    num /= i;
+
+//    i++;
+//    Console.WriteLine(i);
+//}
+
+#endregion
+
+#region SadeBolen
+//Console.WriteLine("eded daxil edin");
+//int eded = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 2; eded >= i; i++)
+//{
+//    while (eded % i == 0)
+//    {
+//        eded /= i;
+//        Console.WriteLine(i);
+//    }
+//}
+#endregion
+
+#region METODS
+//Console.WriteLine("eded daxil edin");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("eded daxil edin");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine("reqemlerin cemi"+" "+Sum(num1,num2));
+//Console.WriteLine("reqemlerin ferqi" + " " + difference(num1, num2));
+
+//if (num2==0)
+//{
+//    Console.WriteLine("0 bolmek olmaz");
+//}
+//else
+//{
+//    Console.WriteLine("reqemlerin nisbeti" + " " + division(num1, num2));
+//}
+
+//Console.WriteLine("reqemlerin hasili" + " " + multiplication(num1, num2));
+//int Sum(int num1,int num2)
+//{
+//    return num1 + num2;
+//}
+//int difference(int num1 , int num2)
+
+//{
+//    return num1 - num2; 
+//}
+//int division(int num1, int num2)
+
+//{
+//    return num1 /num2;
+//}
+//int multiplication(int num1, int num2)
+
+//{
+//    return num1 * num2;
+//}
+
+#endregion
